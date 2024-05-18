41f97956c95864a34638e5e5c244be20892c343e
kchan5071 <145510134+kchan5071@users.noreply.github.com>
2024-05-18 16:36:21 -0700

    first commit

---
+++ install_dependancies.sh
@@ -0,0 +1,12 @@
+sudo apt install python3.11
+sudo apt install python3-pip
+pip3 install yolov5==7.0.13
+pip3 install argparse==1.4.0
+pip3 install inputs==0.5
+pip3 install can==0.0.0
+
+git clone https://github.com/Package-Repository/install.git
+
+wget https://download.stereolabs.com/zedsdk/4.1/l4t36.2/jetsons
+chmod 777 jetsons
+./jetsons
