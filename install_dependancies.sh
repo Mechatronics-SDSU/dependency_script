d7900332d1328fec5fa37e77d4783601b315dc08
kchan5071 <kchan5071@sdsu.edu>
2025-03-14 19:25:05 -0700

    added requirements.txt for python dependencies from pip3

--- install_dependancies.sh
+++ install_dependancies.sh
@@ -1,13 +1,9 @@
+#!/bin/bash
+
 sudo apt install -y python3.11
 sudo apt install -y python3-pip
 sudo apt install -y busybox
-pip3 install yolov5==7.0.13
-pip3 install argparse==1.4.0
-pip3 install inputs==0.5
-pip3 install python-can==3.3.3
-pip3 install cffi==1.16.0
-pip3 install onnx==1.16.1
-pip3 install pdoc3==0.11.1
+pip3 install -r requirements.txt
 
 git clone https://github.com/Mechatronics-SDSU/launch.git
 
